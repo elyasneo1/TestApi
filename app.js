5cff93214ad09ec0eca31cea9afd071ab96f2fe4
Elyas <elyasneo@gmail.com>
2019-06-05 02:08:11 +0430

    begin error handling

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
-
 const app = express();
+const moragn = require('morgan');
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/order')
 
+app.use(moragn("dev"));
 
 app.use('/products', productRoutes)
-app.use('/orders',orderRoutes)
+app.use('/orders', orderRoutes)
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404
+    next(err)
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+         errMsg : err.message  
+    })
+})
 
 
 module.exports = app;
