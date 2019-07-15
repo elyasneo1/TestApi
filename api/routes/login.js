6779679176d7263ec36876409065f0168d46f408
Elyas <elyasneo@gmail.com>
2019-07-16 02:59:35 +0430

    add login

--- api/routes/login.js
+++ api/routes/login.js
@@ -5,6 +5,35 @@ const mongoose = require('mongoose')
 const joi = require('@hapi/joi');
 
 
+router.post('/', async (req, res, next) => {
+    try {
+        joi.validate(req.body, User.userSchema, async (error, value) => {
+            if (!error) {
+                let user = await User.findOne({
+                    username: req.body.username
+                })
+                if (!user) {
+                    res.status(401).json({
+                        msg: "auth failed"
+                    })
+                } else {
+                    res.status(200).json({
+                        msg: "ok"
+                    })
+                }
+            } else {
+                res.status(401).json({
+                    msg: error.details[0].message,
+                })
+            }
+        })
 
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            msg: e
+        })
+    }
+})
 
-module.exports=router
+module.exports = router
